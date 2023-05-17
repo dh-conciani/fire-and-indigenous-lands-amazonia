c058379d9b0e66b320c55e14a83a5dffdfe62fcc
Dhemerson E. Conciani <dhemerson_conciani@yahoo.com.br>
2023-05-17 14:36:43 -0300

    Update 01_ingestTerritories.js

--- 01_ingestTerritories.js
+++ 01_ingestTerritories.js
@@ -3,10 +3,8 @@
 // dhemerson.costa@ipam.org.br
 
 // input territories data as features
-var input = ee.FeatureCollection('users/rayalves/te_amzl_tis_2020_funai_isa_raisg')
-input = ee.FeatureCollection(input.toList(input.size()).slice(0, 19));
-
-var count= 0
+var input = ee.FeatureCollection('users/rayalves/te_amzl_tis_2020_funai_isa_raisg');
+input = ee.FeatureCollection(input.toList(input.size()).slice(0, 400));
 
 // build auxiliary as image
 var input_image = ee.Image(1).clip(input);
@@ -16,13 +14,14 @@ Map.addLayer(input_image);
 var buffer_size = 10000;
 
 // set output imageCollection
-var output = 'users/dh-conciani/help/nucleo_indigena/fire_23/sites';
+var output_folder = 'users/dh-conciani/help/nucleo_indigena/fire_23/';
+var output_name = 'indigenous-lands-image';
 
 // read input data
 var data = ee.ImageCollection(
   input.map(function(feature) {
     // get ocjectid
-    var obj = feature.get('code_funai');
+    var obj = feature.get('id');
     // compute buffer zone
     var buffer = feature.buffer(buffer_size)
       // and retain only difference (outer space)
@@ -35,24 +34,45 @@ var data = ee.ImageCollection(
     // remove overlaps with other territories
     image = image.where(image.eq(2).and(input_image.eq(1)), 0).selfMask();
     
-    return (image);
+    return (image.rename(ee.String(obj)));
   })
 );
 
-print('raw', data);
+// convert to img
+var img = data.toBands().toByte();
 
-// convert to list
-var imageList = data.toList(data.size());
+// Extract the values after underscore and rename the bands in 'img' variable
+var bands = img.bandNames();
+var renamedBands = bands.map(function(band) {
+  var bandName = ee.String(band)
+    .split('_').get(1);  // Extract the value after the underscore
 
-// export each image
-for (var i = 0; i < imageList.length().getInfo(); i++) {
-  var image = ee.Image(imageList.get(i));
-  count = count + 1;
-  Export.image.toAsset({
-        image: image,
-        description: count.toString(),
-        assetId: output + '/' + count.toString(),
-        scale: 10,
-        maxPixels: 1e13
-      });
-}
+  return img.select([band]).rename([bandName]);
+});
+
+// Create a new image with renamed bands
+var renamedImage = renamedBands.slice(1).iterate(function(image, previous) {
+  return ee.Image(previous).addBands(image);
+}, renamedBands.get(0));
+
+// Get the band names of the image
+var bandNames = ee.Image(renamedImage).bandNames();
+
+// Add prefix 'territory' to the start of each band name
+var prefixedBandNames = bandNames.map(function(band) {
+  return ee.String('territory_').cat(band);
+});
+
+// Rename the bands of the image with the prefixed names
+var renamedImage = ee.Image(renamedImage).rename(prefixedBandNames);
+var renameImage = ee.Image(renamedImage).toByte().aside(print);
+
+// stack into a single image
+Export.image.toAsset({
+  image: ee.Image(renamedImage), 
+  description: output_name,
+  assetId: output_folder + output_name,
+  //region: input.geometry(),
+  scale: 10, 
+  maxPixels: 1e13
+});
