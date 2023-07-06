e92e43eea765b98a64b3c492644c49031e5c60a6
Dhemerson E. Conciani <dhemerson_conciani@yahoo.com.br>
2023-07-06 14:24:33 -0300

    Update 04a_getDistanceMatrix.js

--- 04a_getDistanceMatrix.js
+++ 04a_getDistanceMatrix.js
@@ -13,11 +13,103 @@ var territories = ee.ImageCollection('users/dh-conciani/help/nucleo_indigena/fir
 var buffer = territories.updateMask(territories.eq(2));
 
 // get distance mask
-var distance = buffer.distance(ee.Kernel.euclidean(200000, 'meters'), false)
+var distance1 = buffer.distance(ee.Kernel.euclidean(10000, 'meters'), false)
   .updateMask(territories.updateMask(territories.eq(1)));
 
+// get distance mask
+var distance2 = distance1.distance(ee.Kernel.euclidean(10000, 'meters'), false)
+  .updateMask(territories.updateMask(territories.eq(1)))
+  .updateMask(distance1.mask().not())
+  .add(10000);
+
+// get distance mask
+var distance3 = distance2.distance(ee.Kernel.euclidean(10000, 'meters'), false)
+  .updateMask(territories.updateMask(territories.eq(1)))
+  .updateMask(distance1.mask().not())
+  .updateMask(distance2.mask().not())
+  .add(20000);
+
+// get distance mask
+var distance4 = distance3.distance(ee.Kernel.euclidean(10000, 'meters'), false)
+  .updateMask(territories.updateMask(territories.eq(1)))
+  .updateMask(distance1.mask().not())
+  .updateMask(distance2.mask().not())
+  .updateMask(distance3.mask().not())
+  .add(30000);
+
+// get distance mask
+var distance5 = distance4.distance(ee.Kernel.euclidean(10000, 'meters'), false)
+  .updateMask(territories.updateMask(territories.eq(1)))
+  .updateMask(distance1.mask().not())
+  .updateMask(distance2.mask().not())
+  .updateMask(distance3.mask().not())
+  .updateMask(distance4.mask().not())
+  .add(40000);
+
+// get distance mask
+var distance6 = distance5.distance(ee.Kernel.euclidean(10000, 'meters'), false)
+  .updateMask(territories.updateMask(territories.eq(1)))
+  .updateMask(distance1.mask().not())
+  .updateMask(distance2.mask().not())
+  .updateMask(distance3.mask().not())
+  .updateMask(distance4.mask().not())
+  .updateMask(distance5.mask().not())
+  .add(50000);
+
+// get distance mask
+var distance7 = distance6.distance(ee.Kernel.euclidean(10000, 'meters'), false)
+  .updateMask(territories.updateMask(territories.eq(1)))
+  .updateMask(distance1.mask().not())
+  .updateMask(distance2.mask().not())
+  .updateMask(distance3.mask().not())
+  .updateMask(distance4.mask().not())
+  .updateMask(distance5.mask().not())
+  .updateMask(distance6.mask().not())
+  .add(60000);
+
+var distance8 = distance7.distance(ee.Kernel.euclidean(10000, 'meters'), false)
+  .updateMask(territories.updateMask(territories.eq(1)))
+  .updateMask(distance1.mask().not())
+  .updateMask(distance2.mask().not())
+  .updateMask(distance3.mask().not())
+  .updateMask(distance4.mask().not())
+  .updateMask(distance5.mask().not())
+  .updateMask(distance6.mask().not())
+  .updateMask(distance7.mask().not())
+  .add(70000);
+
+var distance9 = distance8.distance(ee.Kernel.euclidean(10000, 'meters'), false)
+  .updateMask(territories.updateMask(territories.eq(1)))
+  .updateMask(distance1.mask().not())
+  .updateMask(distance2.mask().not())
+  .updateMask(distance3.mask().not())
+  .updateMask(distance4.mask().not())
+  .updateMask(distance5.mask().not())
+  .updateMask(distance6.mask().not())
+  .updateMask(distance7.mask().not())
+  .updateMask(distance8.mask().not())
+  .add(80000);
+
+var distance10 = distance9.distance(ee.Kernel.euclidean(10000, 'meters'), false)
+  .updateMask(territories.updateMask(territories.eq(1)))
+  .updateMask(distance1.mask().not())
+  .updateMask(distance2.mask().not())
+  .updateMask(distance3.mask().not())
+  .updateMask(distance4.mask().not())
+  .updateMask(distance5.mask().not())
+  .updateMask(distance6.mask().not())
+  .updateMask(distance7.mask().not())
+  .updateMask(distance8.mask().not())
+  .updateMask(distance9.mask().not())
+  .add(90000);
+
+// merge all
+var matrix = distance1.blend(distance2).blend(distance3).blend(distance4).blend(distance5).blend(distance6).blend(distance7)
+  .blend(distance8).blend(distance9).blend(distance10);
+
+
 Export.image.toAsset({
-		image: distance,
+		image: matrix,
     description: 'DISTANCE_MATRIX',
     assetId: 'users/dh-conciani/help/nucleo_indigena/fire_23/distance_matrix_v2',
     region: geometry,
@@ -26,4 +118,4 @@ Export.image.toAsset({
 });
 
 Map.addLayer(territories.randomVisualizer(), {}, 'all');
-Map.addLayer(distance, {palette:['green', 'yellow', 'red'], min:1, max:40000}, 'distance');
+Map.addLayer(matrix, {palette:['green', 'yellow', 'red'], min:1, max:90000}, 'all');
